782e3d5f80e0ba4dee0d196a7cf8dad10ea1dd9f
Jimmy Rojas <jimmy.rojas@truextend.com>
2020-03-30 11:52:40 -0400

    Connected rest endpoints with services

--- src/main/java/com/organization/springStudentsToClasses/controllers/ClassController.java
+++ src/main/java/com/organization/springStudentsToClasses/controllers/ClassController.java
@@ -2,7 +2,9 @@ package com.organization.springStudentsToClasses.controllers;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+import com.organization.springStudentsToClasses.exceptions.NotFoundException;
 import com.organization.springStudentsToClasses.models.ClassBase;
+import com.organization.springStudentsToClasses.services.ClassSaveService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,27 +21,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("v1/class")
 public class ClassController {
 
+  private final ClassSaveService service;
+
   @Autowired
-  public ClassController() {
+  public ClassController(ClassSaveService service) {
+    this.service = service;
   }
 
   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE, path = "/")
   public ResponseEntity getAllClasses() {
-    return new ResponseEntity(HttpStatus.OK);
+    return new ResponseEntity(this.service.getAll(), HttpStatus.OK);
   }
 
   @RequestMapping(method=POST, value="/")
-  public ResponseEntity createStudent(@RequestBody ClassBase classBase) {
-    return new ResponseEntity(HttpStatus.OK);
+  public ResponseEntity createClass(@RequestBody ClassBase classBase) {
+    return new ResponseEntity(this.service.getAll(), HttpStatus.OK);
   }
 
   @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, path = "/{id}")
-  public ResponseEntity updateStudent(@PathVariable int id, @RequestBody ClassBase classBase) {
-    return new ResponseEntity(HttpStatus.OK);
+  public ResponseEntity updateClass(@PathVariable int id, @RequestBody ClassBase classBase)
+      throws NotFoundException {
+    return new ResponseEntity(this.service.update(id, classBase), HttpStatus.OK);
   }
 
   @DeleteMapping(produces = MediaType.APPLICATION_JSON_VALUE, path = "/{id}")
-  public ResponseEntity deleteStudent(@PathVariable int id) {
+  public ResponseEntity deleteClass(@PathVariable int id)
+      throws NotFoundException {
+    this.service.delete(id);
     return new ResponseEntity(HttpStatus.OK);
   }
 }
--- src/main/java/com/organization/springStudentsToClasses/controllers/StudentController.java
+++ src/main/java/com/organization/springStudentsToClasses/controllers/StudentController.java
@@ -2,7 +2,9 @@ package com.organization.springStudentsToClasses.controllers;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+import com.organization.springStudentsToClasses.exceptions.NotFoundException;
 import com.organization.springStudentsToClasses.models.StudentBase;
+import com.organization.springStudentsToClasses.services.StudentSaveService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,27 +21,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("v1/student")
 public class StudentController {
 
+  private final StudentSaveService service;
+
   @Autowired
-  public StudentController() {
+  public StudentController(StudentSaveService service) {
+    this.service = service;
   }
 
   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE, path = "/")
   public ResponseEntity getAllStudents() {
-    return new ResponseEntity(HttpStatus.OK);
+    return new ResponseEntity(this.service.getAll(), HttpStatus.OK);
   }
 
   @RequestMapping(method=POST, value="/")
-  public ResponseEntity createStudent(@RequestBody StudentBase student) {
-    return new ResponseEntity(HttpStatus.OK);
+  public ResponseEntity createStudent(@RequestBody StudentBase studentBase) {
+    return new ResponseEntity(this.service.save(studentBase), HttpStatus.OK);
   }
 
   @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, path = "/{id}")
-  public ResponseEntity updateStudent(@PathVariable int id, @RequestBody StudentBase student) {
-    return new ResponseEntity(HttpStatus.OK);
+  public ResponseEntity updateStudent(@PathVariable int id, @RequestBody StudentBase studentBase)
+      throws NotFoundException {
+    return new ResponseEntity(this.service.update(id, studentBase), HttpStatus.OK);
   }
 
   @DeleteMapping(produces = MediaType.APPLICATION_JSON_VALUE, path = "/{id}")
-  public ResponseEntity deleteStudent(@PathVariable int id) {
+  public ResponseEntity deleteStudent(@PathVariable int id)
+      throws NotFoundException {
+    this.service.delete(id);
     return new ResponseEntity(HttpStatus.OK);
   }
 }
